5fe491740682e03b2b09d726da061c2a7862f407
kadkm <oakekywephyohann99@gmail.com>
2020-10-20 11:44:06 +0630

    add session

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ app.post('/login',function(req,res){
       res.send('login successful');
     }else{
       res.send('login failed');
-      res.redirect('login');
+      res.render('login');
     }   
 });
 
