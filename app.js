1725874a1931d8a8cb04d98e2acb0dee07b629cb
kadkm <oakekywephyohann99@gmail.com>
2020-10-23 11:20:05 +0630

    te

--- app.js
+++ app.js
@@ -1860,30 +1860,29 @@ FUNCTION TO SET UP PERSISTENT MENU
 ***********************************/
 
 const setupPersistentMenu = (res) => {
-  let messageData = { 
+  var messageData = { 
       "persistent_menu":[
           {
-            "locale": "default",
-            "composer_input_disabled": false,
-            "call_to_actions": [
+            "locale":"default",
+            "composer_input_disabled":false,
+            "call_to_actions":[
                 {
-                    "type": "postback",
-                    "title": "Talk to an agent",
-                    "payload": "CARE_HELP"
+                  "type":"postback",
+                  "title":"View My Tasks",
+                  "payload":"view-tasks"
                 },
                 {
-                    "type": "postback",
-                    "title": "Outfit suggestions",
-                    "payload": "CURATION"
+                  "type":"postback",
+                  "title":"Add New Task",
+                  "payload":"add-task"
                 },
                 {
-                    "type": "web_url",
-                    "title": "Shop now",
-                    "url": "https://www.originalcoastclothing.com/",
-                    "webview_height_ratio": "full"
+                  "type":"postback",
+                  "title":"Cancel",
+                  "payload":"cancel"
                 }
-            ]
-        },
+          ]
+      },
       {
         "locale":"default",
         "composer_input_disabled":false
