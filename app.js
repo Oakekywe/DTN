f54820a1dac3ad5e8e2c72b25a9f3727b83a51ac
kadkm <oakekywephyohann99@gmail.com>
2020-11-09 20:49:02 +0630

    te

--- app.js
+++ app.js
@@ -1436,11 +1436,15 @@ const showOrder = async(sender_psid, order_ref) => {
 
 
 const startReply = (sender_psid) => {
+   let response = {
+                    "recipient":{
+                      "id":sender_psid;
+                    },
+                    "sender_action":"typing_on"
+                  };
    let response1 = {"text": "Welcome to our DTN dessert shop. You can make order online Myanmar traditional dessert with 24/7 services. "};
-   let response2 = {"sender_action":"typing_on",
-   "text": "You can order our delicious menu with loyal points. You can make donation with us. You can also be a loyal member by sign up. "};
+   let response2 = {"text": "You can order our delicious menu with loyal points. You can make donation with us. You can also be a loyal member by sign up. "};
    let response3 = {
-        "sender_action":"typing_on",
          "attachment": {
                 "type": "template",
                 "payload": {
@@ -1487,9 +1491,11 @@ const startReply = (sender_psid) => {
     
  };
 
-  callSend(sender_psid, response1).then(()=>{
-    return callSend(sender_psid, response2).then(()=>{
-      return callSend(sender_psid, response3);
+  callSend(sender_psid, response).then(()=>{
+    return callSend(sender_psid, response1).then(()=>{
+      return callSend(sender_psid, response2).then(()=>{
+        return callSend(sender_psid, response3);
+      });
     });
   });
 }
