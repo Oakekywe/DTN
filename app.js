82b75dc444ffab20171557cd3f1b2426439ba220
kadkm <oakekywephyohann99@gmail.com>
2020-11-13 20:50:54 +0630

    te

--- app.js
+++ app.js
@@ -1219,13 +1219,13 @@ app.get('/emptycart', function(req, res){
 
 app.post('/itempointdiscount', function(req, res){
 
-    if(!temp_points < cart_total){
+    if(temp_points < cart_total){
       customer[user_id].cart = [];
       console.log("BEFORE <<<<<", cart_total);
       console.log("BEFORE <<<<<", temp_points);
         res.send('You dont have sufficient balance. <a href="../showitem">Trade back</a>');
     }
-    else if(temp_points >= cart_total){
+    else(temp_points >= cart_total){
         console.log("AFTER>>>>>>");
         console.log("AFTER>>>>>>", temp_points);
         console.log("AFTER>>>>>>", cart_total);
